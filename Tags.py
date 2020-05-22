1ee92323c52ec89aa43a55f1f7eeeb8c4d27ffc7
SHDW411 <sarotaalan@gmail.com>
2020-05-22 12:29:39 +0200

    v0.3

--- Tags.py
+++ Tags.py
@@ -17,61 +17,6 @@ current_file = "E:\\14 - Together In Electric Dreams.flac"
 FORMATS = ['.mp3', '.wav', '.ogg', '.flac']
 FOLDER_PATH = r'C:\Users\Lenovo PC\Documents\Sound-Traverse'
 
-
-class song_tags:
-
-    def __init__(self, name, title, artist, album, tr_num, date, genre, len, format):
-        self.name = name
-        self.title = title
-        self.artist = artist
-        self.album = album
-        self.tr_num = tr_num
-        self.date = date
-        self.genre = genre
-        self.len = len
-        self.format = format
-
-    #Creates tags base
-    def make_base(self):
-        try:
-            open('tags_base.txt', "r")
-        except FileNotFoundError:
-            open(FOLDER_PATH + r'\\{}'.format('tags_base.txt'), 'w')
-
-    #Read tags and adds song whth its tags to the base
-    def info(currentFile):
-        key_tags = ['name', 'title', 'artist', 'album', 'date', 'genre']
-        curr_tags = {}
-        current_audio = mutagen.File(currentFile)
-        for key in key_tags:
-            curr_key = key.strip('\'')
-            try:
-                curr_tags[curr_key] = str(current_audio[key])
-            except KeyError:
-                curr_tags[curr_key] = "None"
-    #
-    def update_track(path):
-        pass
-
-    def update_folder(path):
-        pass
-
-    def directory_chooser():
-        directory = askdirectory()
-        os.chdir(directory)
-
-        for format in FORMATS:
-            for files in os.listdir(directory):
-                if files.endswith(format):
-                    list_of_songs.append(files)
-
-
-#directory_chooser()
-#make_base()
-#for rec in list_of_songs:
-#    print(rec + "\n")
-
-
 def directory_chooser():
     directory = askdirectory()
     os.chdir(directory)
@@ -82,27 +27,95 @@ def directory_chooser():
                 info(files)
                 list_of_songs.append(files)
 
-def info(currentFile):
-    key_tags = ['name', 'title', 'artist', 'album', 'date', 'genre']
-    curr_tags = {}
-    current_audio = mutagen.File(currentFile)
-    for key in key_tags:
-        curr_key = key.strip('\'')
-        if key == 'name':
-            try:
-                curr_tags[curr_key] = str(currentFile)
-            except KeyError:
-                curr_tags[curr_key] = "None"
-            except TypeError:
-                print("Typ się wywalił z rowerka")
-        else:
-            try:
-                curr_tags[curr_key] = str(current_audio[key])
-            except KeyError:
-                curr_tags[curr_key] = "None"
-            except TypeError:
-                print("Typ się wywalił z rowerka")
-    print(curr_tags)
-    print('\n')
+def make_base():
+    with open(FOLDER_PATH + r'\\{}'.format('tags_base.txt'), 'a') as tempf:
+        tempf.write("name; title; artist; album; tr_num; date; genre: len; format \n")
 
-directory_chooser()
+def info(current_file):
+    flac_tags = ['name', 'title', 'artist', 'album', 'date', 'genre']
+    mp3_tags = ['name', 'title', 'artist', 'album', 'date', 'genre']
+    wav_tags = ['name', 'title', 'artist', 'album', 'date', 'genre']
+    ogg_tags = ['name', 'title', 'artist', 'album', 'date', 'genre']
+    curr_tags = {}
+    tempf = []
+    current_audio = mutagen.File(current_file)
+    if current_file.endswith('.flac'):
+        for key in flac_tags:
+            curr_key = key.strip('\'')
+            if key == 'name':
+                curr_tags[curr_key] = str(current_file)
+                tempf.append(curr_tags[curr_key] + ';')
+            else:
+                try:
+                    curr_tags[curr_key] = str(current_audio[key])
+                    tempf.append(curr_tags[curr_key] + ';')
+                except KeyError:
+                    curr_tags[curr_key] = "None"
+                    tempf.append(curr_tags[curr_key] + ';')
+                except TypeError:
+                    print("Typ się wywalił z rowerka")
+    elif current_file.endswith('.mp3'):
+        for key in mp3_tags:
+            curr_key = key.strip('\'')
+            if key == 'name':
+                curr_tags[curr_key] = str(current_file)
+                tempf.append(curr_tags[curr_key] + ';')
+            else:
+                try:
+                    curr_tags[curr_key] = str(current_audio[key])
+                    tempf.append(curr_tags[curr_key] + ';')
+                except KeyError:
+                    curr_tags[curr_key] = "None"
+                    tempf.write(curr_tags[curr_key] + ';')
+                except TypeError:
+                    print("Typ się wywalił z rowerka")
+    elif current_file.endswith('.wav'):
+        for key in wav_tags:
+            curr_key = key.strip('\'')
+            if key == 'name':
+                curr_tags[curr_key] = str(current_file)
+                tempf.append(curr_tags[curr_key] + ';')
+            else:
+                try:
+                    curr_tags[curr_key] = str(current_audio[key])
+                    tempf.append(curr_tags[curr_key] + ';')
+                except KeyError:
+                    curr_tags[curr_key] = "None"
+                    tempf.append(curr_tags[curr_key] + ';')
+                except TypeError:
+                    print("Typ się wywalił z rowerka")
+    elif current_file.endswith('.ogg'):
+        for key in ogg_tags:
+            curr_key = key.strip('\'')
+            if key == 'name':
+                curr_tags[curr_key] = str(current_file)
+                tempf.append(curr_tags[curr_key] + ';')
+            else:
+                try:
+                    curr_tags[curr_key] = str(current_audio[key])
+                    tempf.append(curr_tags[curr_key] + ';')
+                except KeyError:
+                    curr_tags[curr_key] = "None"
+                    tempf.append(curr_tags[curr_key] + ';')
+                except TypeError:
+                    print("Typ się wywalił z rowerka")
+    try:
+        open(FOLDER_PATH + r'\\{}'.format('tags_base.txt'), 'x')
+    except FileExistsError:
+        f = open("tags_base.txt", "a")
+        for i in tempf:
+            f.write(i)
+        f.close()
+    return curr_tags
+
+def update_track(path):
+    pass
+
+def update_folder(path):
+    pass
+
+#directory_chooser()
+tester = info(current_file)
+
+print(tester)
+print(tester['name'])
